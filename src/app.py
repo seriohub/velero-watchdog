9c050f5327fea2a69e2222d888a2e89fe44b0905
DSerio <davide@serio.cloud>
2025-02-03 19:04:20 +0100

    Updated code

--- src/app.py
+++ src/app.py
@@ -37,31 +37,31 @@ def load_user_config():
                        configmap_name='velero-watchdog-user-config')
     if cm:
         # Update environment variables
-        for key, value in cm.items():
-            print(key, value)
-            os.environ[key] = value
-        # # Update environment variables
         # for key, value in cm.items():
         #     print(key, value)
         #     os.environ[key] = value
-        #
-        # env_file = ".env"
-        # env_data = {}
-        # if os.path.exists(env_file):
-        #     with open(env_file, "r") as f:
-        #         for line in f:
-        #             if "=" in line:
-        #                 key, value = line.strip().split("=", 1)
-        #                 env_data[key] = value  # Manteniamo le vecchie variabili
-        #
-        # # Aggiorna o aggiunge le variabili di os.environ
-        # for key, value in cm.items():
-        #     env_data[key] = value  # Sovrascrive se esiste, altrimenti aggiunge
-        #
-        # # Scrive tutte le variabili aggiornate nel file .env
-        # with open(env_file, "w") as f:
-        #     for key, value in env_data.items():
-        #         f.write(f"{key}={value}\n")
+        # Update environment variables
+        for key, value in cm.items():
+            print(key, value)
+            os.environ[key] = value
+
+        env_file = ".env"
+        env_data = {}
+        if os.path.exists(env_file):
+            with open(env_file, "r") as f:
+                for line in f:
+                    if "=" in line:
+                        key, value = line.strip().split("=", 1)
+                        env_data[key] = value  # Manteniamo le vecchie variabili
+
+        # Aggiorna o aggiunge le variabili di os.environ
+        for key, value in cm.items():
+            env_data[key] = value  # Sovrascrive se esiste, altrimenti aggiunge
+
+        # Scrive tutte le variabili aggiornate nel file .env
+        with open(env_file, "w") as f:
+            for key, value in env_data.items():
+                f.write(f"{key}={value}\n")
     print("\n")
     configHelper = Config()
 
